219bcc7438e5b8def423a81f5a3f4c85a53bbd61
kevin <kevin@cpqd.com.br>
2019-08-06 16:22:54 -0300

    build new

--- travis/publish.sh
+++ travis/publish.sh
@@ -16,4 +16,5 @@ if [ $TRAVIS_PULL_REQUEST == false ] ; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker tag ${TRAVIS_REPO_SLUG} ${tag}
   docker push $tag
+  curl -X POST -H "Content-Type: application/json" --data '{}' https://cloud.docker.com/api/build/v1/source/3d351c62-2904-463c-82cf-d08327c4288f/trigger/269ab73b-5bdc-44e1-a223-1c8bd8a4d235/call/
 fi
